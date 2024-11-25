45656251ec03fe64817a7a6b4a75aa68b1011302
kit-yeung <150399058+kit-yeung@users.noreply.github.com>
2024-11-25 16:43:30 -0500

    add register, login, dashboard, logout routes

--- server/app.py
+++ server/app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify, session
 from flask_mysqldb import MySQL
+from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
+app.secret_key = os.getenv('MY_KEY')
 app.config['MYSQL_HOST'] = os.getenv('MY_HOST')
 app.config['MYSQL_USER'] = os.getenv('MY_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MY_PASSWORD')
@@ -15,15 +18,57 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
-@app.route('/')
-def index():
-    cur = mysql.connection.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS account (name VARCHAR(20) NOT NULL, password VARCHAR(20) NOT NULL, PRIMARY KEY (name))') 
-    cur.execute('INSERT INTO account VALUES ("name", "password")')
-    cur.execute('SELECT * FROM account WHERE name = "name"')
-    account = cur.fetchone()
-    cur.close()
-    return str(account)
+@app.route('/api/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    firstName = data.get('firstName')
+    lastName = data.get('lastName')
+    email = data.get('email')
+    password = data.get('password')
+    hash = generate_password_hash(password)
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM account WHERE email = %s', (email,))
+    user = cursor.fetchone()
+    if user:
+        cursor.close()
+        return jsonify({'error': 'Email already exists'}), 400
+    cursor.execute('INSERT INTO account (firstName, lastName, email, password) VALUES (%s, %s, %s, %s)', (firstName, lastName, email, hash))
+    mysql.connection.commit()
+    cursor.close()
+    return jsonify({'message': 'User registered'}), 200
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM account WHERE email = %s', (email,))
+    user = cursor.fetchone()
+    cursor.close()
+    if user and check_password_hash(user['password'], password):
+        session['isLogin'] = True
+        session['email'] = user['email']
+        return jsonify({'message': 'Login successful'}), 200
+    return jsonify({'error': 'Wrong email or password'}), 400
+
+@app.route('/api/dashboard', methods=['GET'])
+def dashboard():
+    if 'isLogin' in session:
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM account WHERE email = %s', (session['email'],))
+        user = cursor.fetchone()
+        cursor.close()
+        if user:
+            return jsonify({'firstName': user['firstName'], 'lastName': user['lastName']}), 200
+        return jsonify({'error': 'User not found'}), 400
+    return jsonify({'message': 'Unauthorized access'}), 401
+
+@app.route('/api/logout', methods=['POST'])
+def logout():
+    session.pop('isLogin', None)
+    session.pop('email', None)
+    return jsonify({'message': 'Log out successful'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
