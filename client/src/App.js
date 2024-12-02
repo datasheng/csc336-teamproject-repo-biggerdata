5a4ce17be2dec4695fedc115abf1cc0c61bc0005
Desiree Caceres <116980242+desi-C@users.noreply.github.com>
2024-12-02 12:12:06 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from './Components/Login/Login';
-import Reset from './Components/Reset/Reset';
-import Dashboard from './Components/Dashboard/Dashboard';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import Reset from "./Components/Reset/Reset";
+import Homepage from "./Components/Homepage/Homepage";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true); {/* User starts logged in for testing */}
+
   return (
-    <div>
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Login />} />
-          <Route path = "/resetpassword" element={<Reset />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-    </div>
+    <Router>
+      <Routes>
+        {/* Login Route */}
+        <Route
+          path="/"
+          element={
+            isLoggedIn ? (
+              <Navigate to="/homepage" replace />
+            ) : (
+              <Login setIsLoggedIn={setIsLoggedIn} />
+            )
+          }
+        />
+        {/* Homepage Route */}
+        <Route
+          path="/homepage"
+          element={
+            isLoggedIn ? (
+              <Homepage setIsLoggedIn={setIsLoggedIn} />
+            ) : (
+              <Navigate to="/" replace />
+            )
+          }
+        />
+        {/* Reset Password Route */}
+        <Route path="/resetpassword" element={<Reset />} />
+      </Routes>
+    </Router>
   );
 }
 
