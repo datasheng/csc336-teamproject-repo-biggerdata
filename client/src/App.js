0bf8f0fa35b57fb3d408843e43f65f0f0a10c546
kit-yeung <150399058+kit-yeung@users.noreply.github.com>
2024-12-04 19:25:46 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './Components/Login/Login';
 import Reset from './Components/Reset/Reset';
-import Dashboard from './Components/Dashboard/Dashboard';
+import Dashboard from './Components/Homepage/Homepage';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Login />} />
           <Route path = "/resetpassword" element={<Reset />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/homepage" element={<Homepage />} />
         </Routes>
       </BrowserRouter>
     </>
