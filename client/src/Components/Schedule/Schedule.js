ca5c537685e9c835e99a76f098e0c9bc9de8b24f
Desiree Caceres <116980242+desi-C@users.noreply.github.com>
2024-12-13 19:31:48 -0500

    Create Schedule.js

---
+++ client/src/Components/Schedule/Schedule.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Schedule = () => {
+    return (
+      <div className="test">
+        <h1>Scheduling Page Test</h1>
+      </div>
+    );
+  };
+  
+export default Schedule;
