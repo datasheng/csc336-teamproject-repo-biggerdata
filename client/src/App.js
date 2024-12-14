625cd440a67ee06c7b62fa1fd35f90f415128061
Desiree Caceres <116980242+desi-C@users.noreply.github.com>
2024-12-13 19:30:11 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,57 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './Components/Login/Login';
 import Reset from './Components/Reset/Reset';
 import Homepage from './Components/Homepage/Homepage';
+import Browse from './Components/Browse/Browse';
+import Schedule from "./Components/Schedule/Schedule";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true); // User starts logged in for testing
+
   return (
-    <div>
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Login />} />
-          <Route path = "/resetpassword" element={<Reset />} />
-          <Route path="/homepage" element={<Homepage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-    </div>
+    <Router>
+      <Routes>
+        {/* Login Route */}
+        <Route
+          path="/"
+          element={
+            isLoggedIn ? (
+              <Navigate to="/homepage" replace />
+            ) : (
+              <Login setIsLoggedIn={setIsLoggedIn} />
+            )
+          }
+        />
+        {/* Homepage Route */}
+        <Route
+          path="/homepage"
+          element={
+            isLoggedIn ? (
+              <Homepage setIsLoggedIn={setIsLoggedIn} />
+            ) : (
+              <Navigate to="/" replace />
+            )
+          }
+        />
+
+        {/* Reset Password Route */}
+        <Route 
+          path="/resetpassword"
+          element={<Reset />} 
+        />
+
+        {/* Browse Route */}
+        <Route
+          path="/browse"
+          element={<Browse />}
+        />
+
+        {/* Schedule Route */}
+        <Route
+          path="/schedule_build"
+          element={<Schedule />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
