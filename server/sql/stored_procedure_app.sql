0587dc0043903028eb508e0a17d086614b1decff
kit-yeung <150399058+kit-yeung@users.noreply.github.com>
2024-12-16 01:19:10 -0500

    fix error

--- server/sql/stored_procedure_app.sql
+++ server/sql/stored_procedure_app.sql
@@ -1,16 +1,16 @@
 DROP PROCEDURE IF EXISTS RegisterUser;
 CREATE PROCEDURE RegisterUser(
-	IN firstName VARCHAR(50),
-	IN lastName VARCHAR(50),
-    IN email VARCHAR(255),
-    IN password VARCHAR(255))
+    IN firstNameIn VARCHAR(50),
+    IN lastNameIn VARCHAR(50),
+    IN emailIn VARCHAR(255),
+    IN passwordIn VARCHAR(255))
 BEGIN
     INSERT INTO account (firstName, lastName, email, password)
-    VALUES (firstName, lastName, email, password);
+    VALUES (firstNameIn, lastNameIn, emailIn, passwordIn);
 END;
 
 DROP PROCEDURE IF EXISTS CheckEmail;
-CREATE PROCEDURE CheckEmail(IN email VARCHAR(255))
+CREATE PROCEDURE CheckEmail(IN emailIn VARCHAR(255))
 BEGIN
-    SELECT * FROM account WHERE email = email;
+    SELECT * FROM account WHERE email = emailIn;
 END;
